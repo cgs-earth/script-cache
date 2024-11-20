45453e7db6bf48dd7dc74c8fa37c4d05d5f0a53e
Benjamin Webb <40066515+webb-ben@users.noreply.github.com>
2024-11-20 12:38:24 -0500

    Update install_caddy.sh

--- install_caddy.sh
+++ install_caddy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Start Caddy install!" > /var/log/startup.log
+echo "Start Caddy install!" >> /var/log/startup.log
 apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
