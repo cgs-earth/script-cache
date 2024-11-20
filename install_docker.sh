1e7dfbe42329602f5b5c7f03b7f6273b61338a23
Benjamin Webb <40066515+webb-ben@users.noreply.github.com>
2024-11-20 14:40:57 -0500

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -2,6 +2,7 @@
 echo "Start Docker install!" >> /var/log/startup.log
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
+sudo usermod -aG docker $USER
 systemctl enable docker
 systemctl start docker
 echo "Docker started!" >> /var/log/startup.log
