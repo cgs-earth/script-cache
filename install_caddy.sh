1ab66a785fcf64842e71e74ff8fa30a4c970858a
Benjamin Webb <40066515+webb-ben@users.noreply.github.com>
2024-12-16 15:34:32 -0500

    Update install_caddy.sh

--- install_caddy.sh
+++ install_caddy.sh
@@ -5,6 +5,8 @@ curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --d
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
 apt update
 apt install -y caddy
+sudo chmod 755 /usr/bin/caddy
+sudo setcap CAP_NET_BIND_SERVICE=+eip /usr/bin/caddy
 systemctl enable caddy
 systemctl start caddy
 echo "Caddy started!" >> /var/log/startup.log
