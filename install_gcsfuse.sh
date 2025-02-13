df4dc3bbdbf37ea51b276e2d4a5c8dabff9936cf
Benjamin Webb <benjamin.miller.webb@gmail.com>
2025-02-12 19:52:43 -0500

    Update install_gcsfuse.sh

--- install_gcsfuse.sh
+++ install_gcsfuse.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # Update and upgrade system packages
-sudo apt update
+sudo apt-get update
 
 # Set up key
-echo "deb http://packages.cloud.google.com/apt gcsfuse-bullseye main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.asc] https://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /usr/share/keyrings/cloud.google.asc
 
 # Install fuse
-sudo apt install gcsfuse
+sudo apt-get install gcsfuse
 
 # Log success message
 echo "gcsfuse installation complete" >> /var/log/startup.log
