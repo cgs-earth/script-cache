84b82456b4c644791143f2d2de7070722d4a2375
Benjamin Webb <benjamin.miller.webb@gmail.com>
2026-05-07 21:05:48 -0400

    Update install_gcsfuse.sh

--- install_gcsfuse.sh
+++ install_gcsfuse.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-# Update and upgrade system packages
-sudo apt-get update
+export GCSFUSE_REPO=gcsfuse-$(lsb_release -c -s)
+echo "deb https://packages.cloud.google.com/apt $GCSFUSE_REPO main" \
+    | sudo tee /etc/apt/sources.list.d/gcsfuse.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg \
+    | sudo apt-key add -
 
-# Set up key
-export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.asc] https://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /usr/share/keyrings/cloud.google.asc
-
-# Install fuse
-sudo apt-get update && sudo apt-get -y install gcsfuse
-sudo chmod +x /usr/bin/gcsfuse
-echo "user_allow_other" | sudo tee -a /etc/fuse.conf
-
-# Log success message
-echo "gcsfuse installation complete" >> /var/log/startup.log
+# Update and install gcsfuse
+sudo apt update
+sudo apt install gcsfuse
