b1e6af64ed8e9c751911e04be088bccdd4a293d8
Benjamin Webb <40066515+webb-ben@users.noreply.github.com>
2026-05-11 14:53:02 -0400

    Move qlever-control install to script-cache (#2)

--- install_qlever.sh
+++ install_qlever.sh
@@ -4,6 +4,12 @@
 apt-get update
 apt-get install -y wget gnupg ca-certificates curl python3
 
+# Install qlever-control
+python3 -m venv --system-site-packages /venv
+git clone https://github.com/ad-freiburg/qlever-control
+cd qlever-control
+/venv/bin/python3 -m pip install -e ".[dev]"
+
 # Add QLever repository
 wget -qO - https://packages.qlever.dev/pub.asc | gpg --dearmor > /usr/share/keyrings/qlever.gpg && \
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/qlever.gpg] https://packages.qlever.dev/ $(. /etc/os-release && echo ${VERSION_CODENAME}) main" > /etc/apt/sources.list.d/qlever.list
